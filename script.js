8553ab783197551be96937a32c78cccfac5c1c63
James Phillips <scorpi4n@scorpi4n.com>
2022-09-21 11:58:49 -0500

    Add 'read' attribute to Book

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let myLibrary = []
 
-function Book(title, author) {
+function Book(title, author, read) {
 	this.title = title
 	this.author = author
+	this.read = read
 }
 
 function addBookToLibrary(book) {
