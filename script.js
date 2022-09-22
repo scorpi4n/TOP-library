678cab05d5325a698a0b6dac77a81e68450ac910
James Phillips <scorpi4n@scorpi4n.com>
2022-09-22 15:33:20 -0500

    Add functionality to 'add new book' button

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let myLibrary = []
 
-function Book(title, author, read) {
+function Book(title, author, read, pages, rating, genre) {
 	this.title = title
 	this.author = author
 	this.read = read
+	this.pages = pages
+	this.rating = rating
+	this.genre = genre
 }
 
 function addBookToLibrary(book) {
 	myLibrary.push(book)
 }
+
+let addBookBtn = document.getElementById('add-book')
+addBookBtn.addEventListener('click', function() {
+	let modal = document.getElementById('input-modal')
+	if (modal.style.display == 'block') {
+		modal.style.display = 'none'
+		addBookBtn.innerText = 'add new book'
+	} else {
+		modal.style.display = 'block'
+		addBookBtn.innerText = 'close'
+	}
+})
